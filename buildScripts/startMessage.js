fe1d7d2cec3adcb6cd87c4981b75e0812c17d97f
benjamin.hide <ben.m.hide@googlemail.com>
2019-08-16 16:32:29 +0100

    final commit of the linting module

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,4 @@
 import { green } from "chalk";
 
+/* eslint-disable no-console */
 console.log(green("Starting app in dev mode..."));
--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
 import "./index.css";
-
-const value = 1000;
-debugger;
-console.log(`I would pay ${value} for this awesome app!`);
